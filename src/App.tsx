5b33dd52c67f13dc9264218a8fc750156791ce47
Danny Pesses <dcpesses@users.noreply.github.com>
2024-02-27 13:49:42 -0800

    feat(todos): updated component instance within App

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,6 @@ import logo from '@/assets/logo.svg';
 function App() {
   return (
     <div className="App">
-      <div className="App-Todos">
-        <h2>Todos</h2>
-        <Todo />
-      </div>
-
-      <hr />
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
@@ -51,6 +44,13 @@ function App() {
           </a>
         </p>
       </header>
+
+      <hr />
+
+      <div className="App-Todos">
+        <h2>Todos</h2>
+        <Todo />
+      </div>
     </div>
   );
 }
