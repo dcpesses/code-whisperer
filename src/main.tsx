7b47b57c0dfeab07982e01aae1ce0a21d275a25d
Danny Pesses <dcpesses@users.noreply.github.com>
2024-03-10 11:02:28 -0700

    refactor(auth): disabled React StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
@@ -9,12 +9,20 @@ import './index.css';
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <HashRouter>
+//         <App />
+//       </HashRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 );
