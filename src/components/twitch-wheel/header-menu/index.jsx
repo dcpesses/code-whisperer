868fdc75a1e099d8b681255f5db04535b611d345
Danny Pesses <dcpesses@users.noreply.github.com>
2024-04-01 12:55:46 -0700

    refactor(header-menu): add reusable functions to change/update settings, update unit-tests

--- src/components/twitch-wheel/header-menu/index.jsx
+++ src/components/twitch-wheel/header-menu/index.jsx
@@ -2,7 +2,6 @@
 import {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button, Collapse, Dropdown} from 'react-bootstrap';
-// import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -15,10 +14,13 @@ import {version} from '../../../../package.json';
 
 import './header-menu.css';
 
+export const noop = () => void 0;
+
 export class HeaderMenu extends Component {
   static get propTypes() {
     return {
-      channelInfo:PropTypes.object,
+      clearAllQueues: PropTypes.func,
+      channelInfo: PropTypes.object,
       debugItems: PropTypes.array,
       // gamesList: PropTypes.object,
       moderatedChannels: PropTypes.array,
@@ -34,6 +36,7 @@ export class HeaderMenu extends Component {
   }
   static get defaultProps() {
     return {
+      clearAllQueues: noop,
       channelInfo: {},
       debugItems: [],
       gamesList: {
@@ -41,12 +44,12 @@ export class HeaderMenu extends Component {
         validGames: null
       },
       moderatedChannels: [],
-      onLogout: () => void 0,
-      onSettingsUpdate: () => void 0,
+      onLogout: noop,
+      onSettingsUpdate: noop,
       settings: {},
-      setChannelInfo: () => void 0,
-      showModalCommandList: () => void 0,
-      toggleChangelogModal: () => void 0,
+      setChannelInfo: noop,
+      showModalCommandList: noop,
+      toggleChangelogModal: noop,
       twitchApi: null,
       userInfo: {},
     };
@@ -60,28 +63,31 @@ export class HeaderMenu extends Component {
     };
     this.toggleGameList = this.toggleGameList.bind(this);
     this.toggleSettingsMenu = this.toggleSettingsMenu.bind(this);
+
+    this.updateCustomDelimiter = this.updateInputOption.bind(this, 'customDelimiter');
+    this.updateCustomJoinCommand = this.updateInputOption.bind(this, 'customJoinCommand');
+    this.updateCustomLeaveCommand = this.updateInputOption.bind(this, 'customLeaveCommand');
+
+    this.toggleEnableRoomCode = this.toggleOption.bind(this, 'enableRoomCode');
+    this.toggleJoinConfirmationMessage = this.toggleOption.bind(this, 'enableJoinConfirmationMessage');
+    this.toggleLeaveConfirmationMessage = this.toggleOption.bind(this, 'enableLeaveConfirmationMessage');
+    this.toggleEnableModeratedChannelsOption = this.toggleOption.bind(this, 'enableModeratedChannelsOption');
   }
 
-  /*
+  /**
+   * Creates an array of DropdownItems using a given object array
+   * @param {array} items Array of objects to transform into DropdownItems; Each object contains:
+   *   `label` {string} Label to display on the DropdownItem,
+   *   `onClick` {function} Callback fired when the DropdownItem is clicked
+   * @param {string} activeLabel (optional) Label of the DropdownItem to highlight as active/selected.
+   * @returns array of DropdownItems
+   */
+  createDropdownItems = (items, activeLabel=null) => {
+    if (!items) {return [];}
 
-    Expects the following object schema:
-    {
-        label: string,  // required
-        onClick: func,
-        listItemClassName: string,
-        btnClassName: string
-    }
-
-
-     */
-  createDebugMenuItems = (items, activeLabel=null) => {
-    if (!items) {
-      return [];
-    }
     return items.map((i, idx) => {
-      if (!i || !i.label) {
-        return null;
-      }
+      if (!i || !i.label) {return null;}
+      // create divider if label only contains dashes
       if (/^[-]+$/i.test(i.label)) {
         return (
           <Dropdown.Divider key={i.idx} eventKey={i.idx} />
@@ -101,6 +107,11 @@ export class HeaderMenu extends Component {
     }).filter(i => i);
   };
 
+  /**
+   * Generates the menu items to display in the Moderated Channels dropdown
+   * @param {string} activeLabel (optional) Label of the DropdownItem to highlight as active/selected.
+   * @returns JSX.Element of Dropdown Menu Items
+   */
   createModeratedChannelsMenuItems = (activeLabel) => {
     if (!this.props.moderatedChannels) {
       return null;
@@ -122,9 +133,9 @@ export class HeaderMenu extends Component {
         <Dropdown.Header>
           Moderated Channels
         </Dropdown.Header>
-        {this.createDebugMenuItems(items.slice(0, 1), activeLabel)}
+        {this.createDropdownItems(items.slice(0, 1), activeLabel)}
         <Dropdown.Divider />
-        {this.createDebugMenuItems(items.slice(1), activeLabel)}
+        {this.createDropdownItems(items.slice(1), activeLabel)}
       </>
     );
   };
@@ -132,121 +143,68 @@ export class HeaderMenu extends Component {
   onModeratedChannelMenuItem = async(channel) => {
     try {
       let channelInfo = await this.props.twitchApi.switchChannel(channel.broadcaster_login);
-      if (!channelInfo) {
-        channelInfo = this.props.twitchApi.channelInfo;
-      }
-      this.props.setChannelInfo(channelInfo.data[0]);
+      channelInfo = (!channelInfo) ? this.props.channelInfo : channelInfo.data[0];
+
+      this.props.setChannelInfo(channelInfo);
+      this.props.clearAllQueues();
       return;
     } catch (e) {
       console.error(`getModeratedChannelsMenu - ${channel.broadcaster_name} - error`, e);
     }
   };
 
-
-  createMenuItems = (items) => {
-    if (!items) {return [];}
-    return items.map(i => {
-      if (!i.label) {return null;}
-      return (
-        <Nav.Link key={i.label} onClick={i.onClick || null}>
-          {i.label}
-        </Nav.Link>
-      );
-    }).filter(i => i);
+  sanitizeInputEventTargetValue = (e) => {
+    let {value} = e.target;
+    if (!value) {
+      return null;
+    } else {
+      return value.trim();
+    }
   };
 
-  toggleGameList = () => {
-    this.setState((state) => {
-      return {
-        showGameList: !state.showGameList
-      };
-    });
+  toggleGameList = () => this.setState((state) => ({showGameList: !state.showGameList}));
+
+  toggleSettingsMenu = () => this.setState((state) => ({showSettingsMenu: !state.showSettingsMenu}));
+
+  /**
+   * Toggles the value of the specified option in the settings
+   * @param {string} optionName Name of the setting to change
+   * @param {boolean} defaultValue (optional) Value to assign if not yet initialized. Default: `true`
+   */
+  toggleOption = (optionName, defaultValue=true) => {
+    if (typeof optionName !== 'string') {return;}
+    const {settings, onSettingsUpdate} = this.props;
+    const value = (typeof settings?.[optionName] === 'boolean')
+      ? !settings[optionName]
+      : defaultValue;
+    onSettingsUpdate({[optionName]: value});
+    return;
   };
 
-  toggleSettingsMenu = () => {
-    this.setState((state) => {
-      return {
-        showSettingsMenu: !state.showSettingsMenu
-      };
-    });
+  updateInputOption = (optionName, e) => {
+    if (typeof optionName !== 'string' || !e?.target) {return;}
+    this.props.onSettingsUpdate({[optionName]: this.sanitizeInputEventTargetValue(e)});
+    return;
   };
+  /*
+  const toggleSubRequests = this.toggleOption.bind(this, 'enableSubRequests');
+  const toggleSubRequestLimithis.toggleOption.bind(this, 'enableSubRequestLimit');
+  const toggleClearSeatsAftethis.toggleOption.bind(this, 'clearSeatsAfterRedeem');
+  */
+  // updateCustomDelimiter = (e) => {
+  //   this.props.onSettingsUpdate({customDelimiter: this.sanitizeInputEventTargetValue(e)});
+  // };
+  // updateCustomJoinCommand = (e) => {
+  //   this.props.onSettingsUpdate({customJoinCommand: this.sanitizeInputEventTargetValue(e)});
+  // };
+  // updateCustomLeaveCommand = (e) => {
+  //   this.props.onSettingsUpdate({customLeaveCommand: this.sanitizeInputEventTargetValue(e)});
+  // };
+
 
   render() {
-    let {channelInfo, debugItems, userInfo, settings, onSettingsUpdate} = this.props;
-    let debugMenuItems = this.createDebugMenuItems(debugItems);
-
-    /*
-    let toggleSubRequests = () => {
-      let value = typeof settings?.enableSubRequests === 'boolean'
-        ? !settings?.enableSubRequests
-        : true;
-      onSettingsUpdate({enableSubRequests: value});
-    };
-    let toggleSubRequestLimit = () => {
-      let value = typeof settings?.enableSubRequestLimit === 'boolean'
-        ? !settings?.enableSubRequestLimit
-        : true;
-      onSettingsUpdate({enableSubRequestLimit: value});
-    };
-    let toggleClearSeatsAfterRedeem = () => {
-      let value = typeof settings?.clearSeatsAfterRedeem === 'boolean'
-        ? !settings?.clearSeatsAfterRedeem
-        : true;
-      onSettingsUpdate({clearSeatsAfterRedeem: value});
-    };
-    */
-    let updateCustomDelimiter = (e) => {
-      let {value} = e.target;
-      if (!value) {
-        value = null;
-      } else {
-        value = value.trim();
-      }
-      onSettingsUpdate({customDelimiter: value});
-    };
-    let updateCustomJoinCommand = (e) => {
-      let {value} = e.target;
-      if (!value) {
-        value = null;
-      } else {
-        value = value.trim();
-      }
-      onSettingsUpdate({customJoinCommand: value});
-    };
-    let updateCustomLeaveCommand = (e) => {
-      let {value} = e.target;
-      if (!value) {
-        value = null;
-      } else {
-        value = value.trim();
-      }
-      onSettingsUpdate({customLeaveCommand: value});
-    };
-    let toggleEnableRoomCode = () => {
-      let value = typeof settings?.enableRoomCode === 'boolean'
-        ? !settings?.enableRoomCode
-        : true;
-      onSettingsUpdate({enableRoomCode: value});
-    };
-    let toggleJoinConfirmationMessage = () => {
-      let value = typeof settings?.enableJoinConfirmationMessage === 'boolean'
-        ? !settings?.enableJoinConfirmationMessage
-        : true;
-      onSettingsUpdate({enableJoinConfirmationMessage: value});
-    };
-    let toggleLeaveConfirmationMessage = () => {
-      let value = typeof settings?.enableLeaveConfirmationMessage === 'boolean'
-        ? !settings?.enableLeaveConfirmationMessage
-        : true;
-      onSettingsUpdate({enableLeaveConfirmationMessage: value});
-    };
-
-    let toggleEnableModeratedChannelsOption = () => {
-      let value = typeof settings?.enableModeratedChannelsOption === 'boolean'
-        ? !settings?.enableModeratedChannelsOption
-        : true;
-      onSettingsUpdate({enableModeratedChannelsOption: value});
-    };
+    let {channelInfo, debugItems, userInfo, settings/*, onSettingsUpdate*/} = this.props;
+    const debugMenuItems = this.createDropdownItems(debugItems);
 
     // user may not always be same as broadcaster/channel
     let selectedUserInfo = channelInfo;
@@ -320,7 +278,7 @@ export class HeaderMenu extends Component {
                   <div id="settings-menu" className="accordion-dark accordion accordion-flush">
                     <div className="accordion-body">
                       <Button variant="link" id="enable-room-code" className="btn settings-menu"
-                        onClick={toggleEnableRoomCode}
+                        onClick={this.toggleEnableRoomCode}
                         title="Allows host to set a room code that can be whispered to players."
                       >
                         <input type="checkbox" role="switch" checked={(settings?.enableRoomCode)} readOnly /> <span>Enable Room Code</span>
@@ -354,11 +312,11 @@ export class HeaderMenu extends Component {
                         <input type="text" id="custom-join-command" name="custom-join-command"
                           placeholder="e.g. !join"
                           defaultValue={settings?.customJoinCommand}
-                          onChange={updateCustomJoinCommand} className="form-control" spellCheck="false" />
+                          onChange={this.updateCustomJoinCommand} className="form-control" spellCheck="false" />
                       </Button>
 
                       <Button variant="link" className="btn settings-menu"
-                        onClick={toggleJoinConfirmationMessage}
+                        onClick={this.toggleJoinConfirmationMessage}
                         title="Messages users in chat when they've successfully joined the Interested queue."
                       >
                         <input type="checkbox" role="switch"
@@ -372,11 +330,11 @@ export class HeaderMenu extends Component {
                         <input type="text" id="custom-leave-command" name="custom-leave-command"
                           placeholder="e.g. !leave"
                           defaultValue={settings?.customLeaveCommand}
-                          onChange={updateCustomLeaveCommand} className="form-control" spellCheck="false" />
+                          onChange={this.updateCustomLeaveCommand} className="form-control" spellCheck="false" />
                       </Button>
 
                       <Button variant="link" className="btn settings-menu"
-                        onClick={toggleLeaveConfirmationMessage}
+                        onClick={this.toggleLeaveConfirmationMessage}
                         title="Messages users in chat when they've successfully left the queues."
                       >
                         <input type="checkbox" role="switch"
@@ -389,7 +347,7 @@ export class HeaderMenu extends Component {
                         <span htmlFor="custom-delimiter">Use Custom Delimiter: </span>
                         <input type="text" id="custom-delimiter" name="custom-delimiter"
                           defaultValue={settings?.customDelimiter}
-                          onChange={updateCustomDelimiter} className="form-control" spellCheck="false" />
+                          onChange={this.updateCustomDelimiter} className="form-control" spellCheck="false" />
                       </Button>
 
 
@@ -401,7 +359,7 @@ export class HeaderMenu extends Component {
                       </div>
 
                       <Button variant="link" id="enable-moderated-channels-option" className="btn settings-menu"
-                        onClick={toggleEnableModeratedChannelsOption}
+                        onClick={this.toggleEnableModeratedChannelsOption}
                         title="Allows user to use this app on another channel that grants them moderation access."
                       >
                         <input type="checkbox" role="switch" checked={(settings?.enableModeratedChannelsOption)} readOnly /> <span>Enable Moderated Channels Menu</span>
