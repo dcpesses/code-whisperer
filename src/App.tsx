34521e7f2721773db08394f73f174948d25a18f0
Danny Pesses <dcpesses@users.noreply.github.com>
2025-02-06 12:42:42 -0800

    refactor(App): created separate Error404 page, updated unit-tests to properly detect routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Link, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // Pages
 import AuthenticatedApp from '@/pages/authenticated-app';
+import Error404 from '@/pages/error404';
 import Login from '@/features/login';
 import Landing from '@/pages/landing';
 
@@ -15,25 +16,6 @@ declare global {
   }
 }
 
-const Error404 = () => (
-  <div className="error-404 full-pg d-flex w-100 h-100 my-0 mx-auto flex-column">
-    <main className="px-3 text-center">
-      <h1 className="display-1 ppb-4">404</h1>
-      <p>
-        <small className="fst-italic">
-          I&apos;m sorry, Dave. I&apos;m afraid I can&apos;t do that.
-        </small>
-      </p>
-      <div className="col-6 mx-auto">
-        <Link className="btn btn-outline-primary btn-lg mt-5" to="/">
-          Go Home
-        </Link>
-      </div>
-    </main>
-  </div>
-);
-
-
 function App() {
   return (
     <Routes>
@@ -46,3 +28,4 @@ function App() {
 }
 
 export default App;
+export {Error404};
