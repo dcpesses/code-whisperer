129e86e07fcada1584b3a6b2e6b0afa4821641ec
Danny Pesses <dcpesses@users.noreply.github.com>
2025-02-07 18:19:23 -0800

    fix(login): updated uncaught linting errors

--- src/features/login/login.test.jsx
+++ src/features/login/login.test.jsx
@@ -12,31 +12,31 @@ vi.mock('../../../package.json', () => {
 });
 
 describe('getLoginUrl', () => {
-    test('should return twitch oauth url', () => {
-      vi.stubEnv('VITE_APP_TWITCH_CLIENT_ID', 'VITE_APP_TWITCH_CLIENT_ID');
-      vi.stubEnv('VITE_APP_TWITCH_CLIENT_SECRET', 'VITE_APP_TWITCH_CLIENT_SECRET');
-      vi.stubEnv('VITE_APP_REDIRECT_URI', 'VITE_APP_REDIRECT_URI');
+  test('should return twitch oauth url', () => {
+    vi.stubEnv('VITE_APP_TWITCH_CLIENT_ID', 'VITE_APP_TWITCH_CLIENT_ID');
+    vi.stubEnv('VITE_APP_TWITCH_CLIENT_SECRET', 'VITE_APP_TWITCH_CLIENT_SECRET');
+    vi.stubEnv('VITE_APP_REDIRECT_URI', 'VITE_APP_REDIRECT_URI');
 
-      expect(getLoginUrl()).toEqual('https://id.twitch.tv/oauth2/authorize?client_id=VITE_APP_TWITCH_CLIENT_ID&response_type=code&scope=chat:read chat:edit moderation:read user:manage:whispers moderator:read:chatters user:read:moderated_channels channel:read:vips channel:read:editors moderator:manage:announcements user:read:subscriptions&redirect_uri=VITE_APP_REDIRECT_URI');
-    });
+    expect(getLoginUrl()).toEqual('https://id.twitch.tv/oauth2/authorize?client_id=VITE_APP_TWITCH_CLIENT_ID&response_type=code&scope=chat:read chat:edit moderation:read user:manage:whispers moderator:read:chatters user:read:moderated_channels channel:read:vips channel:read:editors moderator:manage:announcements user:read:subscriptions&redirect_uri=VITE_APP_REDIRECT_URI');
+  });
 });
 
 describe('Login', () => {
   describe('clearLocalStorageData', () => {
-      test('should call setState', () => {
-        vi.useFakeTimers({ toFake: ['nextTick'] });
-        const component = new Login();
-        vi.spyOn(component, 'setState').mockImplementation(()=>{});
-        vi.spyOn(global, 'setTimeout');
-        component.clearLocalStorageData();
-        vi.advanceTimersByTime(5500);
-        expect(global.setTimeout).toHaveBeenCalledTimes(1);
-        global.setTimeout.mock.calls[0][0]();
-        // console.log({calls: global.setTimeout.mock.calls[0][0]})
+    test('should call setState', () => {
+      vi.useFakeTimers({ toFake: ['nextTick'] });
+      const component = new Login();
+      vi.spyOn(component, 'setState').mockImplementation(()=>{});
+      vi.spyOn(global, 'setTimeout');
+      component.clearLocalStorageData();
+      vi.advanceTimersByTime(5500);
+      expect(global.setTimeout).toHaveBeenCalledTimes(1);
+      global.setTimeout.mock.calls[0][0]();
+      // console.log({calls: global.setTimeout.mock.calls[0][0]})
 
-        expect(component.setState).toHaveBeenCalledTimes(2);
-        vi.useRealTimers();
-      });
+      expect(component.setState).toHaveBeenCalledTimes(2);
+      vi.useRealTimers();
+    });
   });
   describe('render', () => {
     beforeEach(()=>{
@@ -55,7 +55,7 @@ describe('Login', () => {
     });
     test('Should render and handle modal events', async() => {
       vi.useFakeTimers({ toFake: ['nextTick'] });
-      const {container} = render(
+      render(
         <Login />
       );
       const btnResetAppData = screen.getByText('Having issues?');
