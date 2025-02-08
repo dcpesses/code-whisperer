dcad2e6c8167dfec9e3870543b6bf7425b1931ab
Danny Pesses <dcpesses@users.noreply.github.com>
2025-02-07 18:13:15 -0800

    fix(landing): added mock version to unit-test for snapshot

--- src/pages/landing/landing.test.tsx
+++ src/pages/landing/landing.test.tsx
@@ -1,6 +1,13 @@
 import {render} from '@testing-library/react';
 import Landing from './index';
 
+vi.mock('../../../package.json', () => {
+  return {
+    ...vi.importActual('../../../package.json'),
+    version: '0.0.0'
+  };
+});
+
 describe('Landing', () => {
   test('Should render as expected', () => {
     const {container} = render(<Landing />);
