052f6db198b669ef07310df20277f9c61facc657
Danny Pesses <dcpesses@users.noreply.github.com>
2025-02-12 19:08:04 -0800

    feat(chat-bubbles): updated colors/opacity/default props, added fadein, updated snapshot

--- src/components/chat-bubbles/index.jsx
+++ src/components/chat-bubbles/index.jsx
@@ -2,20 +2,21 @@ import PropTypes from 'prop-types';
 
 import './chat-bubbles.css';
 
-function ChatBubbles({ count=24, size=16, maxHeight=80, maxWidth=100 }) {
+function ChatBubbles({ count=48, size=36, maxHeight=75, maxWidth=100 }) {
 
-  const stars = Array.from({ length: count }, (_, i) => {
+  const chats = Array.from({ length: count }, (_, i) => {
 
     const randBlink = Math.floor(Math.random() * 5) + 1;
-    const randSize = Math.max(2, Math.floor(Math.random() * size) - 1);
+    const randSize = Math.max(12, Math.floor(Math.random() * size) - 1);
 
     return (
       <div
         className={`chat-bubble blink_${randBlink}`}
         key={`chat-bubble_${i}_${randBlink}`} style={{
           fontSize: `${randSize}px`,
-          top: `calc(${Math.random() * maxHeight - 1}vh - ${randSize}px)`,
+          top: `calc(${Math.random() * maxHeight - 1}vh - ${randSize}px - 125px)`,
           left: `calc(${Math.random() * maxWidth}vw - ${randSize}px)`,
+          opacity: Math.max(randSize / size, 0.5),
         }}>
         <i className="bi bi-chat-square" />
       </div>
@@ -23,10 +24,8 @@ function ChatBubbles({ count=24, size=16, maxHeight=80, maxWidth=100 }) {
   });
 
   return (
-    <div className="chat-bubbles" style={{height: `${maxHeight}vh`}}>
-
-      {stars}
-
+    <div className="chat-bubbles" style={{height: `calc(${maxHeight}vh - 125px)`}}>
+      {chats}
     </div>
   );
 }
@@ -37,9 +36,9 @@ ChatBubbles.propTypes = {
   size: PropTypes.number,
 };
 ChatBubbles.defaultProps = {
-  count: 24,
-  maxHeight: 80,
+  count: 16,
+  maxHeight: 75,
   maxWidth: 100,
-  size: 16,
+  size: 64,
 };
 export default ChatBubbles;
