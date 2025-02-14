39132971aa8018ab6fa2f5fa0b8a5f51f3c81909
Danny Pesses <dcpesses@users.noreply.github.com>
2025-02-13 23:38:40 -0800

    refactor(chat-bubbles): updated to use different animation method, added starfield, updated snapshot

--- src/components/chat-bubbles/chat-bubbles.test.tsx
+++ src/components/chat-bubbles/chat-bubbles.test.tsx
@@ -3,18 +3,7 @@ import ChatBubbles from './index';
 
 describe('ChatBubbles', () => {
   test('Should render as expected', () => {
-    vi.spyOn(Math, 'random')
-      .mockReturnValueOnce(0.9)
-      .mockReturnValueOnce(0.1)
-      .mockReturnValueOnce(0.8)
-      .mockReturnValueOnce(0.2)
-      .mockReturnValueOnce(0.7)
-      .mockReturnValueOnce(0.3)
-      .mockReturnValueOnce(0.6)
-      .mockReturnValueOnce(0.4)
-      .mockReturnValue(0.5);
-    const {container} = render(<ChatBubbles count={4} size={16} />);
-    expect(Math.random).toBeCalledTimes(16);
+    const {container} = render(<ChatBubbles />);
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/chat-bubbles/index.jsx
+++ src/components/chat-bubbles/index.jsx
@@ -1,44 +1,16 @@
-import PropTypes from 'prop-types';
-
 import './chat-bubbles.css';
 
-function ChatBubbles({ count=48, size=36, maxHeight=75, maxWidth=100 }) {
-
-  const chats = Array.from({ length: count }, (_, i) => {
-
-    const randBlink = Math.floor(Math.random() * 5) + 1;
-    const randSize = Math.max(12, Math.floor(Math.random() * size) - 1);
-
-    return (
-      <div
-        className={`chat-bubble blink_${randBlink}`}
-        key={`chat-bubble_${i}_${randBlink}`} style={{
-          fontSize: `${randSize}px`,
-          top: `calc(${Math.random() * maxHeight - 1}vh - ${randSize}px - 125px)`,
-          left: `calc(${Math.random() * maxWidth}vw - ${randSize}px)`,
-          opacity: Math.max(randSize / size, 0.5),
-        }}>
-        <i className="bi bi-chat-square" />
-      </div>
-    );
-  });
+function ChatBubbles() {
 
   return (
-    <div className="chat-bubbles" style={{height: `calc(${maxHeight}vh - 125px)`}}>
-      {chats}
+    <div className="chat-bubbles">
+      <div id="stars1" className="stars" />
+      <div id="stars2" className="stars" />
+      <div id="stars3" className="stars" />
+      <div id="chats1" className="chats">&#xF265;</div>
+      <div id="chats2" className="chats">&#xF265;</div>
     </div>
   );
 }
-ChatBubbles.propTypes = {
-  count: PropTypes.number,
-  maxHeight: PropTypes.number,
-  maxWidth: PropTypes.number,
-  size: PropTypes.number,
-};
-ChatBubbles.defaultProps = {
-  count: 16,
-  maxHeight: 75,
-  maxWidth: 100,
-  size: 64,
-};
+
 export default ChatBubbles;
